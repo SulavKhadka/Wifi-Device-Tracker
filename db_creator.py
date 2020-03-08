bd55212111342a2cf6c7c72b0921835d7f649244
Sulav Khadka <khadkasulav@gmail.com>
2020-03-08 13:16:40 -0400

    added logging.

--- db_creator.py
+++ db_creator.py
@@ -1,10 +1,26 @@
 import os
 import csv
+import sys
 import sqlite3
+import logging
 from collections import defaultdict
 
 class Database(object):
     def __init__(self, db_path):
+        # Logging setup
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        file_handler = logging.FileHandler(f"{__name__}.log", mode='a')
+        formatter = logging.Formatter('%(asctime)s :: %(filename)s :: %(levelname)s :: %(message)s')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        consoleHandler = logging.StreamHandler(sys.stdout)
+        consoleHandler.setLevel(logging.INFO)
+        consoleHandler.setFormatter(formatter)
+        logger.addHandler(consoleHandler)
+
+        self.logger = logger
         self.db_path = db_path
         self.table_info = None
         self.conn = None
@@ -30,32 +46,33 @@ class Database(object):
     def make_db(self, table_info=None):
 
         if os.path.exists(self.db_path):
-            print("Database already exists. Opening...") # :TODO: log this
+            self.logger.info("Database already exists. Opening...")
         else:
-            print(f"Creating database at {self.db_path}")
+            self.logger.info(f"Creating database at {self.db_path}")
 
         db_creator_conn = sqlite3.connect(self.db_path)
-        print("Database connected")
+        self.logger.info("Database connected")
 
         c = db_creator_conn.cursor()
         if table_info:
-            print("Found table_info. Making db tables...")
+            self.logger.info("Found table_info. Making db tables...")
 
             # :TODO: Move this logic elsewhere
             for table_name, column_info in table_info.items():
                 try:
                     query_string = f"CREATE TABLE IF NOT EXISTS {table_name} ({column_info})"
-                    print(query_string)
+                    self.logger.debug(f"Query: {query_string}")
                     c.execute(query_string)
                 except sqlite3.OperationalError:
-                    print(f"{table_name} already exists")
+                    self.logger.debug(f"{table_name} already exists")
             
             db_creator_conn.commit()
         db_creator_conn.close()
+        self.logger.info(f"Database created. Path: {self.db_path}")
     
     def connect(self, create=False, db_schema_path=None):
         if not os.path.exists(self.db_path):
-            print(f"Database not found at {self.db_path}") # :TODO: log this
+            self.logger.info(f"Database not found at {self.db_path}")
             if create:
                 if db_schema_path:
                     with open(db_schema_path, "r") as schema_file:
@@ -63,9 +80,11 @@ class Database(object):
                         try:
                             table_info = self.parse_table_info(table_info_csv)
                         except:
+                            self.logger.exception(f"Unable to parse database schema file. Path: {db_schema_path}")
                             raise Exception(f"Unable to parse database schema file. Path: {db_schema_path}")
                         self.make_db(table_info)
                 else:
+                    self.logger.exception("'db_schema_path' flag is needed for 'create' flag to be True")
                     raise Exception("'db_schema_path' flag is needed for 'create' flag to be True")
             else:
                 return
--- network_device_tracker.py
+++ network_device_tracker.py
@@ -2,11 +2,13 @@ import sys
 import time
 import signal
 import sqlite3
+import logging
 import subprocess
 from datetime import datetime, timezone
 from functools import partial
 
 import db_creator
+
 
 def get_devices_on_network():
     nmap = subprocess.Popen(['arp', '-a'], stdout=subprocess.PIPE)
@@ -24,8 +26,8 @@ def get_devices_on_network():
             "MacAddress": raw_list[3]
             })
         except Exception as e:
-            print("Uh Oh! Something is wrong with this line.")
-            print(f"Line: {raw_list} \n Error: {e}")
+            logger.error(f"Line: {raw_list} \n Error: {e}")
+            logger.exception(f"Zoinks!")
     
     return devices
 
@@ -36,11 +38,16 @@ def log_devices(db, commit_frequency=10, scan_time_interval=30):
     cursor = db.conn.cursor()
     current_log_frequency = 0
 
+    logger.info("Starting network scan...")
+    logger.info(f"Commit frequency: {commit_frequency}")
+    logger.info(f"Scan-time interval: {scan_time_interval}")
+
     while True:
         scan_start_time = time.time() # Recording start and stop times for the sleep mechanism below
 
         timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
         devices = get_devices_on_network()
+        logger.info(f"{len(devices)} device(s) found.")
 
         for device in devices:
             try:
@@ -48,36 +55,49 @@ def log_devices(db, commit_frequency=10, scan_time_interval=30):
                                     VALUES (?,?,?,?,?);'''
                 values = list(device.values())
                 values.append(timestamp)
+                logger.debug(f"Insert values: {values}")
                 
-                print(f"Values: {values}") #:TODO: Log this
                 cursor.execute(query_string, tuple(values))
-                print("Record logged.") #:TODO: Log this
+                logger.debug("Record logged.")
             except Exception as e:
-                print(f"Unable to execute statement.")
+                logger.exception("Unable to execute statement.")
                 raise Exception(e)
-
-        scan_end_time = time.time()
-        
-        # Sleep mechanism to make sure the device scans happen at the scan_time_interval points
-        time_till_scan_time_interval = scan_time_interval - (scan_end_time - scan_start_time)
-        if time_till_scan_time_interval > 0:
-            time.sleep(time_till_scan_time_interval)
         
         if current_log_frequency == commit_frequency:
             db.conn.commit()
         current_log_frequency += 1
 
-        print(f"scan time: {time.time() - scan_start_time}") #:TODO: log this
+        scan_end_time = time.time()
+        logger.info(f"Scan time: {scan_end_time - scan_start_time}")
 
+        # Sleep mechanism to make sure the device scans happen at the scan_time_interval points
+        time_till_scan_time_interval = scan_time_interval - (scan_end_time - scan_start_time)
+        if time_till_scan_time_interval > 0:
+            time.sleep(time_till_scan_time_interval)
 
 def terminate_handler(self, signum, frame):
     # Graceful exit handling logic
+    self.logger.info("Recieved kill signal, Closing database.")
     print("\nRecieved kill signal, Closing database.")
     self.conn.close()
     sys.exit(0)
 
 
 if __name__ == "__main__":
+
+    # Logging setup
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(f"{__name__}.log", mode='a')
+    formatter = logging.Formatter('%(asctime)s :: %(filename)s :: %(levelname)s :: %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setLevel(logging.INFO)
+    consoleHandler.setFormatter(formatter)
+    logger.addHandler(consoleHandler)
+
     db = db_creator.Database(db_path="network_device_logs.db")
     signal.signal(signal.SIGINT, partial(terminate_handler, db)) # Graceful exit handling call
     log_devices(db, commit_frequency=2, scan_time_interval=20)
