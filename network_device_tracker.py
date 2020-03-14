e9cb92f297468c530912171bc471a359905f8206
Sulav Khadka <khadkasulav@gmail.com>
2020-03-14 17:48:57 -0400

    Better logs and DB schema changes

--- network_device_tracker.py
+++ network_device_tracker.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import json
+import uuid
 import signal
 import sqlite3
 import logging
@@ -10,101 +12,194 @@ from functools import partial
 import db_creator
 
 
-def get_devices_on_network():
-    nmap = subprocess.Popen(['arp', '-a'], stdout=subprocess.PIPE)
-    ipout = nmap.communicate()[0].decode("utf-8")
-    
-    devices = []
-    for i in ipout.strip().split("\n"):
+class DeviceTracker():
+
+    def __init__(self, db_path):
+        # Logging setup
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        file_name = __file__.split(".")[0]
+        file_handler = logging.FileHandler(f"{file_name}.log", mode='a')
+        formatter = logging.Formatter('%(asctime)s :: %(filename)s :: %(levelname)s :: %(message)s')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        consoleHandler = logging.StreamHandler(sys.stdout)
+        consoleHandler.setLevel(logging.INFO)
+        consoleHandler.setFormatter(formatter)
+        logger.addHandler(consoleHandler)
+
+
+        db = db_creator.Database(db_path=db_path)
+        db.connect(create=True, db_schema_path="db_schema.csv")
+
+        self.device_file = open("devices.json", "r+")
+        if self.device_file is not None:
+            self.devices = json.load(self.device_file)
+        else:
+            self.devices = {}
+        self.logger = logger
+        self.db = db
+        self.conn = self.db.conn
+        self.cursor = None
+        if self.conn is None:
+            self.logger.error("Cannot get cursor. self.conn is None.")
+            raise Exception("Cannot get cursor. self.conn is None.")
+        self.cursor = self.conn.cursor()
+        self.all_devices = {
+            "device_correlation" : {},
+            "device_ips_set" : set(),
+            "device_macs_set" : set()
+        }
         
-        raw_list = i.split(" ")
-        try:
-            devices.append({
-            "AssignedDeviceName" : "",
-            "IdentifiedDeviceName": raw_list[0],
-            "IpAddress": raw_list[1][1:-1],
-            "MacAddress": raw_list[3]
-            })
-        except Exception as e:
-            logger.error(f"Line: {raw_list} \n Error: {e}")
-            logger.exception(f"Zoinks!")
-    
-    return devices
 
 
-def log_devices(db, commit_frequency=10, scan_time_interval=30):
-    
-    db.connect(create=True, db_schema_path="db_schema.csv")
-    cursor = db.conn.cursor()
-    total_logs = 0
-    current_log_frequency = 0
+    def get_devices_on_network(self):
+        nmap = subprocess.Popen(['arp', '-a'], stdout=subprocess.PIPE)
+        ipout = nmap.communicate()[0].decode("utf-8")
+        
+        devices = []
+        for i in ipout.strip().split("\n"):
+            add_device = False
+            raw_list = i.split(" ")
 
-    logger.info("Starting network scan...")
-    logger.info(f"Commit frequency: {commit_frequency}")
-    logger.info(f"Scan-time interval: {scan_time_interval}")
+            # Correlating the device to the db device ID.
+            name = self.all_devices.get('device_correlation').get(raw_list[1][1:-1])
+            if name is None:
+                name = self.all_devices.get('device_correlation').get(raw_list[3])
+                if name is None:
+                    self.logger.warning(f"Unable to find device id from IP: {raw_list[1][1:-1]} | MAC: {raw_list[3]}.")
+                    name = f"device{uuid.uuid4()}"
+                    add_device = True
 
-    while True:
-        scan_start_time = time.time() # Recording start and stop times for the sleep mechanism below
-
-        timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
-        devices = get_devices_on_network()
-        logger.info(f"{len(devices)} device(s) found.")
-
-        for device in devices:
             try:
-                query_string = f'''INSERT INTO devices(name,arpIdentifiedName,ipAddress,macAddress,timestamp) 
-                                    VALUES (?,?,?,?,?);'''
-                values = list(device.values())
-                values.append(timestamp)
-                logger.debug(f"Insert values: {values}")
-                
-                cursor.execute(query_string, tuple(values))
-                logger.debug("Record logged.")
+                device = {
+                "AssignedDeviceName" : name,
+                "IdentifiedDeviceName": raw_list[0],
+                "IpAddress": raw_list[1][1:-1],
+                "MacAddress": raw_list[3]
+                }
+
             except Exception as e:
-                logger.exception("Unable to execute statement.")
-                raise Exception(e)
+                self.logger.error(f"Line: {raw_list} \n Error: {e}")
+                self.logger.exception(f"Zoinks!")
+
+            if add_device:
+                self.add_new_device(device)
+            
+            devices.append(device)
         
-        if current_log_frequency == commit_frequency:
-            logger.info(f"Committing {current_log_frequency} records.")
-            db.conn.commit()
-            current_log_frequency = -1
-        current_log_frequency += 1
-        total_logs += 1
+        return devices
 
-        scan_end_time = time.time()
-        logger.info(f"Record #{total_logs}")
-        logger.info(f"Scan time: {scan_end_time - scan_start_time}")
 
-        # Sleep mechanism to make sure the device scans happen at the scan_time_interval points
-        time_till_scan_time_interval = scan_time_interval - (scan_end_time - scan_start_time)
-        if time_till_scan_time_interval > 0:
-            time.sleep(time_till_scan_time_interval)
+    def add_device_to_file(self, device):
+        device_name = device.get('AssignedDeviceName')
+        self.logger.debug(f"Adding {device_name} to file.")
+
+        try:
+            self.devices[device_name] = {
+                "name": device_name,
+                "arpIdentifiedName": device.get('IdentifiedDeviceName'),
+                "ipAddress" : device.get('IpAddress'),
+                "macAddress" : device.get('MacAddress')
+            }
+            self.logger.debug(f"{device_name} written to file")
+        except:
+            self.logger.exception(f"Failed to write {device_name} to file.")
+
+
+    def add_device_to_db(self, device):
+        device_name = device.get('AssignedDeviceName')
+        query_string = f'''INSERT INTO devices(name,arpIdentifiedName,ipAddress,macAddress) 
+                            VALUES (?,?,?,?);'''
+        values = list(device.values())
+        self.logger.debug(f"Insert values: {values}")
+        
+        try:
+            self.cursor.execute(query_string, tuple(values))
+            self.logger.debug(f"{device_name} added to db")
+        except:
+            self.logger.exception(f"Failed to add {device_name} to db.")
+
+
+    def get_all_devices(self):
+        query_string = f'''SELECT id,ipAddress,macAddress FROM devices '''       
+        self.cursor.execute(query_string)
+        devices = self.cursor.fetchall()
+        self.all_devices['device_correlation'] = {i[1]:i[0] for i in devices}
+        self.all_devices['device_correlation'] = {i[2]:i[0] for i in devices}
+        self.all_devices['device_ips_set'] = set([i[1] for i in devices])
+        self.all_devices['device_macs_set'] = set([i[2] for i in devices])
+
+
+    def add_new_device(self, device):
+        self.logger.info("New device found.")
+        self.logger.debug(device)
+
+        self.add_device_to_file(device)
+        self.add_device_to_db(device)
+        self.get_all_devices()
+
+
+    def log_devices(self, commit_frequency=10, scan_time_interval=30):
+        
+        total_logs = 0
+        current_log_frequency = 0
+
+        self.logger.info("Starting network scan...")
+        self.logger.info(f"Commit frequency: {commit_frequency}")
+        self.logger.info(f"Scan-time interval: {scan_time_interval}")
+
+        while True:
+            scan_start_time = time.time() # Recording start and stop times for the sleep mechanism below
+
+            timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+            devices = self.get_devices_on_network()
+            self.logger.info(f"{len(devices)} device(s) found.")
+            
+            for device in devices:
+
+                try:
+                    query_string = f'''INSERT INTO devicelogs(deviceId,timestamp) 
+                                        VALUES (?,?);'''
+                    values = [device.get('AssignedDeviceName')]
+                    values.append(timestamp)
+                    self.logger.debug(f"Insert values: {values}")
+                    
+                    self.cursor.execute(query_string, tuple(values))
+                    self.logger.debug("Record logged.")
+                except Exception as e:
+                    self.logger.exception("Unable to execute statement.")
+                    raise Exception(e)
+            
+            if current_log_frequency == commit_frequency:
+                self.logger.info(f"Committing {current_log_frequency} records.")
+                self.db.conn.commit()
+                current_log_frequency = -1
+            current_log_frequency += 1
+            total_logs += 1
+
+            scan_end_time = time.time()
+            self.logger.info(f"Record #{total_logs}")
+            self.logger.info(f"Scan time: {scan_end_time - scan_start_time}")
+
+            # Sleep mechanism to make sure the device scans happen at the scan_time_interval points
+            time_till_scan_time_interval = scan_time_interval - (scan_end_time - scan_start_time)
+            if time_till_scan_time_interval > 0:
+                time.sleep(time_till_scan_time_interval)
 
 def terminate_handler(self, signum, frame):
     # Graceful exit handling logic
     self.logger.info("Recieved kill signal, Closing database.")
     print("\nRecieved kill signal, Closing database.")
-    self.conn.commit()
-    self.conn.close()
+    json.dump(self.devices, self.device_file, indent = 4, sort_keys=True)
+    self.device_file.close()
+    self.db.conn.commit()
+    self.db.conn.close()
     sys.exit(0)
 
 
 if __name__ == "__main__":
-
-    # Logging setup
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    file_name = __file__.split(".")[0]
-    file_handler = logging.FileHandler(f"{file_name}.log", mode='a')
-    formatter = logging.Formatter('%(asctime)s :: %(filename)s :: %(levelname)s :: %(message)s')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    consoleHandler = logging.StreamHandler(sys.stdout)
-    consoleHandler.setLevel(logging.INFO)
-    consoleHandler.setFormatter(formatter)
-    logger.addHandler(consoleHandler)
-
-    db = db_creator.Database(db_path="network_device_logs.db")
-    signal.signal(signal.SIGINT, partial(terminate_handler, db)) # Graceful exit handling call
-    log_devices(db, commit_frequency=5, scan_time_interval=10)
+    tracker = DeviceTracker(db_path="network_device_logs.db")
+    signal.signal(signal.SIGINT, partial(terminate_handler, tracker)) # Graceful exit handling call
+    tracker.log_devices(commit_frequency=5, scan_time_interval=10)
